492f5783b048e3adfc7fe4f449b1a1bea55a385b
moritanian <pickit2express@gmail.com>
2017-06-03 01:30:06 +0900

    webVR setting basic機能

---
+++ three/webVR/WebVRSetting.js
@@ -0,0 +1,219 @@
+/*
+	VR化セットアップクラス
+
+	EventDispathcer使ってVRコントローラの入力とれるようにしたい
+	https://threejs.org/docs/#api/core/EventDispatcher
+
+*/
+var WebVRSetting = {
+	init: function(renderer, camera, scene, path){
+		
+		var Instance = this;
+		Object.assign(this, THREE.EventDispatcher.prototype);
+
+		this.controls =  new THREE.VRControls( camera );
+		this.controls.standing = true;
+
+		this.controller1 = new THREE.ViveController( 0 );
+		this.controller1.standingMatrix = this.controls.getStandingMatrix();
+		this.controller1.addEventListener( 'triggerdown', function(event){
+			Instance.dispatchEvent({type: "triggerchange", trigger: "down", controller: Instance.controller1, triggerEvent: event});
+		});
+		this.controller1.addEventListener( 'triggerup', function(event){
+			Instance.dispatchEvent({type: "triggerchange", trigger: "up", controller: Instance.controller1, triggerEvent: event});
+		});
+		scene.add( this.controller1 );
+
+		this.controller2 = new THREE.ViveController( 1 );
+		this.controller2.standingMatrix = this.controls.getStandingMatrix();
+		this.controller2.addEventListener( 'triggerdown', function(event){
+			Instance.dispatchEvent({type: "triggerchange", trigger: "down", controller: Instance.controller2, triggerEvent: event});
+		});
+		this.controller2.addEventListener( 'triggerup', function(event){
+			Instance.dispatchEvent({type: "triggerchange", trigger: "up", controller: Instance.controller2, triggerEvent: event});
+		});
+		scene.add( this.controller2 );
+
+		var loader = new THREE.OBJLoader();
+		loader.setPath( 'https://threejs.org/examples/models/obj/vive-controller/' );
+		loader.load( 'vr_controller_vive_1_5.obj', function ( object ) {
+
+			var loader = new THREE.TextureLoader();
+			loader.setPath( path );
+
+			var controller = object.children[ 0 ];
+			controller.material.map = loader.load( 'onepointfive_texture.png' );
+			controller.material.specularMap = loader.load( 'onepointfive_spec.png' );
+
+			Instance.controller1.add( object.clone() );
+			Instance.controller2.add( object.clone() );
+
+		} );
+
+		var geometry = new THREE.Geometry();
+		geometry.vertices.push( new THREE.Vector3( 0, 0, 0 ) );
+		geometry.vertices.push( new THREE.Vector3( 0, 0, - 1 ) );
+
+		var line = new THREE.Line( geometry );
+		line.name = 'line';
+		line.scale.z = 5;
+
+		this.controller1.add( line.clone() );
+		this.controller2.add( line.clone() );
+
+		this.raycaster = new THREE.Raycaster();
+
+		this.effect = new THREE.VREffect( renderer );
+
+		WEBVR.getVRDisplay( function ( display ) {
+
+			document.body.appendChild( WEBVR.getButton( display, renderer.domElement ) );
+
+		} );
+
+		window.addEventListener( 'resize', onWindowResize, false );
+		
+		this.intersected = [];
+
+		this.tempMatrix = new THREE.Matrix4();
+
+		this.intersectableObjects = new THREE.Group();
+		// シーンに追加する必要ある？
+		scene.add(this.intersectableObjects);
+
+		function onWindowResize() {
+
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+
+			Instance.effect.setSize( window.innerWidth, window.innerHeight );
+
+		}
+
+	},
+
+
+	setIntersectableObjects: function(objects){
+		for(let i=0; i<objects.length; i++){
+			this.intersectableObjects.add(objects[i]);
+		}
+	},
+
+	cleanIntersected: function(){
+
+		while ( this.intersected.length ) {
+
+			var object = this.intersected.pop();
+			object.material.emissive.r = 0;
+
+		}
+
+	},
+
+	// コントローラに指示されているオブジェクト取得
+	getIntersections: function( controller ) {
+
+		this.tempMatrix.identity().extractRotation( controller.matrixWorld );
+
+		this.raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
+		this.raycaster.ray.direction.set( 0, 0, -1 ).applyMatrix4( this.tempMatrix );
+
+		return this.raycaster.intersectObjects( this.intersectableObjects.children );
+
+	},
+
+	// 支持しているオブジェクトに向かってビーム
+	intersectObjects: function( controller ) {
+
+		// Do not highlight when already selected
+
+		if ( controller.userData.selected !== undefined ) return;
+
+		var line = controller.getObjectByName( 'line' );
+		var intersections = this.getIntersections( controller );
+
+		if ( intersections.length > 0 ) {
+
+			var intersection = intersections[ 0 ];
+
+			var object = intersection.object;
+			object.material.emissive.r = 1;
+			this.intersected.push( object );
+
+			line.scale.z = intersection.distance;
+
+		} else {
+
+			line.scale.z = 5;
+
+		}
+
+	},
+
+	attachIntersectedObject: function(controller){
+		
+		var intersections = this.getIntersections(controller);
+
+		if ( intersections.length > 0 ) {
+
+			var intersection = intersections[ 0 ];
+
+			var object = intersection.object;
+
+			this.attachObject(controller, object);
+		}
+		
+	},
+
+	attachObject : function(controller, object){
+		this.tempMatrix.getInverse( controller.matrixWorld );
+
+		object.matrix.premultiply( this.tempMatrix );
+		object.matrix.decompose( object.position, object.quaternion, object.scale );
+		object.material.emissive.b = 1;
+		controller.add( object );
+
+		controller.userData.selected = object;
+
+	},
+
+	detachObject: function(controller){
+		if ( controller.userData.selected !== undefined ) {
+
+			var object = controller.userData.selected;
+			object.matrix.premultiply( controller.matrixWorld );
+			object.matrix.decompose( object.position, object.quaternion, object.scale );
+			object.material.emissive.b = 0;
+			this.intersectableObjects.add( object );
+
+			controller.userData.selected = undefined;
+
+		}
+
+
+	},
+
+	// should be called in animate()
+	startLoop: function(scene, camera, animate){
+		var Instance = this;
+		var animateFunc = function(){
+			Instance.effect.requestAnimationFrame(animateFunc);
+			animate();
+			Instance.controller1.update();
+			Instance.controller2.update();
+
+			Instance.controls.update();
+
+			Instance.cleanIntersected();
+
+			Instance.intersectObjects( Instance.controller1 );
+			Instance.intersectObjects( Instance.controller2 );
+			Instance.effect.render( scene, camera );
+
+		};
+
+		animateFunc();
+
+	}
+
+}
